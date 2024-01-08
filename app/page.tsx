321a3a011e83afe020ca79ba7d79f0a942249ca1
hafizn07 <hafiznjh007@gmail.com>
2024-01-08 20:20:12 +0530

    feat: homapage ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
 import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
-    <Button>
-      Click Me
-    </Button>
-  )
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-cyan-500 to-blue-500">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service!</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign In
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
 }
