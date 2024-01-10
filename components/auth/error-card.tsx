986678a21a8a43b72078c12181aab134f039fbb3
hafizn07 <hafiznjh007@gmail.com>
2024-01-10 14:41:24 +0530

    feat: add OAuth (Google & Github)

---
+++ components/auth/error-card.tsx
@@ -0,0 +1,16 @@
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+
+export const ErrorCard = () => {
+  return (
+    <CardWrapper
+      headerLabel="Oops! Something went wrong!"
+      backButtonHref="/auth/login"
+      backButtonLabel="Back to login"
+    >
+      <div className="w-full flex items-center justify-center">
+        <ExclamationTriangleIcon className="text-destructive" />
+      </div>
+    </CardWrapper>
+  );
+};
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as z from "zod";
+import { useSearchParams } from "next/navigation";
 import { useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -21,6 +22,12 @@ import { FormSuccess } from "@/components/form-success";
 import { login } from "@/actions/login";
 
 export const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const errorUrl =
+    searchParams.get("error") === "OAuthAccountNotLinked"
+      ? "Email already in use with a different provider"
+      : "";
+
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
@@ -39,8 +46,9 @@ export const LoginForm = () => {
 
     startTransition(() => {
       login(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
+        // TODO: Add when add 2FA
+        // setSuccess(data?.success);
       });
     });
   };
@@ -92,7 +100,7 @@ export const LoginForm = () => {
               )}
             />
           </div>
-          <FormError message={error} />
+          <FormError message={error || errorUrl} />
           <FormSuccess message={success} />
           <Button type="submit" disabled={isPending} className="w-full">
             Login
